fae288e214c42ea259062da78168cf8273365a75
alina villarreal <alina.michelle1@gmail.com>
2018-08-13 18:00:32 +0100

    fixed the migration problem started the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+
   resources :users do
-    resources :messages, only: [:new, :create] #
+    resources :wishlist, only: [:new, :create]
+  end
+     #
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
