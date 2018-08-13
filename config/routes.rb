17615d1bfe68e61d105ab23cd174d745265ccc9e
John Tarver <john@atriptyc.com>
2018-08-13 18:01:32 +0100

    created wishlist model

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     resources :messages, only: [:new, :create] #
   devise_for :users
   root to: 'pages#home'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
