4453e4bb8133471f66d85150c1fb4ea4663885de
alina villarreal <alina.michelle1@gmail.com>
2018-08-14 15:20:14 +0100

    commented what i did

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
-  def index
-  end
+  # def index
+  # end
 
-  def show
-    if user_signed_in?
-      @user = current_user
-    end
+  # def show
+  #   if user_signed_in?
+  #     @user = current_user
+  #   end
 
-    @expertises = Expertise.all
-    @wishlists = Wishlist.all
-    @saves = Save.all
-  end
+  #   @expertises = Expertise.all
+  #   @wishlists = Wishlist.all
+  #   @saves = Save.all
+  # end
 
 end
