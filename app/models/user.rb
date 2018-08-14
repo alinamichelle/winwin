5a16dfdd87d87b1e1c45f96d922f72a2224d3354
alina villarreal <alina.michelle1@gmail.com>
2018-08-14 15:45:23 +0100

    fixed typo on validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :location, presence: true
   validates :birthday, presence: true
-  alidates_length_of :tagline, :minimum => 15, :maximum => 20, presence: true
+  validates_length_of :tagline, :minimum => 15, :maximum => 20, presence: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
