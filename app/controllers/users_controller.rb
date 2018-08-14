75e684ab8a5986bd48e1fe216d917c9ca6384950
alina villarreal <alina.michelle1@gmail.com>
2018-08-14 15:19:16 +0100

    updated users_controller, began user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
+  def index
+  end
+
   def show
     if user_signed_in?
       @user = current_user
     end
-  end
 
-  @expertises = Expertise.all
-  @wishlists = Wishlist.all
-  @saves = Save.all
+    @expertises = Expertise.all
+    @wishlists = Wishlist.all
+    @saves = Save.all
+  end
 
 end
