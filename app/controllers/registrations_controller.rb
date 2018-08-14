9ad2f005540365a9fea87466d6dd4ded6457696d
alina villarreal <alina.michelle1@gmail.com>
2018-08-14 17:22:16 +0100

    fixed the devise reroute

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,9 @@ class RegistrationsController < Devise::RegistrationsController
 
   protected
 
+  def after_sign_up_path_for(resource)
+    '/accounts/edit' # Or :prefix_to_your_route
+  end
   def update_resource(resource, params)
     # Require current password if user is trying to change password.
     return super if params["password"]&.present?
