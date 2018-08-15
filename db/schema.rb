aa20af94195fe501bc63f9c00b44aa0a06c4de48
alina villarreal <alina.michelle1@gmail.com>
2018-08-15 14:16:15 +0100

    fixed the wishes iteration

--- db/schema.rb
+++ db/schema.rb
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2018_08_15_115959) do
     t.text "biography"
     t.integer "role"
     t.integer "gender"
+    t.string "avatar"
     t.string "avatar_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
