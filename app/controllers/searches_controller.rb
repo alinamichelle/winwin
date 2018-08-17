4b2a5049ea47753abed6ab10034c33bd3c3a9051
John Tarver <john@atriptyc.com>
2018-08-17 14:58:58 +0100

    finalized search code

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,17 @@
 class SearchesController < ApplicationController
   def index
-    q1 = params.require(:query).permit(:query)
-    if q1[:query].present?
-      topic_ids = Topic.where(name: q1[:query]).pluck :id
+    if params[:search].present?
+      topic_ids = Topic.where("name ILIKE ?", "%#{params[:query]}").pluck :id
       user_ids = Expertise.where("topic_id in (#{topic_ids.join(",")})").pluck :user_id
       @results = User.where("id in (#{user_ids.join(",")})")
-      redirect_to search_path
-    else
-      @users = User.all
+    # else
+    #   @users = User.all
     end
   end
+
+  # private
+
+  # def search_params
+  #   params.require(:search).permit(:name)
+  # end
 end
