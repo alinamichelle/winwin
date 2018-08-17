0388d48b27d457e21eb3157d5ac3b60ccc01848f
John Tarver <john@atriptyc.com>
2018-08-17 16:52:38 +0100

    resolved image rendering issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-s# This file should contain all the record creation needed to seed the database with its default values.
+# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
@@ -6,7 +6,7 @@ s# This file should contain all the record creation needed to seed the database 
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
-
+Expertise.destroy_all
 Topic.destroy_all
 User.destroy_all
 
